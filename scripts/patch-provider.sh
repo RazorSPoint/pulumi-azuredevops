46b1c48a25046a46bb45b1cad637412143e2775e
Thomas Meckel <tmeckel@users.noreply.github.com>
2020-04-22 18:25:48 +0000

    First compilable version

---
+++ scripts/patch-provider.sh
@@ -0,0 +1,16 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+base_dir="${DIR}/../provider"
+patch_dir="${DIR}/../patch"
+
+cd $base_dir || exit 1
+
+go mod tidy || exit 1
+go mod vendor || exit 1
+
+cd vendor/github.com/microsoft/azure-devops-go-api/ || exit 1
+patch --verbose -p1 -i $patch_dir/azure-devops-go-api-rune-error.patch || exit 1
+
+cd $base_dir || exit 1
+cd vendor/github.com/hashicorp/terraform-plugin-sdk/ || exit 1
+patch --verbose -p1 -i $patch_dir/terraform-plugin-sdk-requiredwith.patch || exit 1
+cd $base_dir || exit 1
