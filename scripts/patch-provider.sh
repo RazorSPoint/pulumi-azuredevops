ba59a80a8e51d8303b5f2039e9c512ba598ebb32
Thomas Meckel <tmeckel@users.noreply.github.com>
2020-05-19 19:01:02 +0200

    Removed obsolete patches from scripts/patch-provider.sh

--- scripts/patch-provider.sh
+++ scripts/patch-provider.sh
@@ -3,9 +3,3 @@ base_dir="${DIR}/../provider"
 patch_dir="${DIR}/../patch"
 
 cd $base_dir || exit 1
-
-go mod tidy || exit 1
-go mod vendor || exit 1
-
-cd vendor/github.com/microsoft/azure-devops-go-api/ || exit 1
-patch --quiet -p1 -i $patch_dir/azure-devops-go-api-rune-error.patch || exit 1
